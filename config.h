ee9858c73db041ed71fbbbb957d13dde49303da0
leekool <leejedda@gmail.com>
2022-11-07 22:38:43 +1100

    cursor warp

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 3;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "Tamzen:pixelsize=15" };
-static const char dmenufont[]       = "Tamzen:pixelsize=15";
+static const char *fonts[]          = { "PragmataPro Mono:size=12" };
+static const char dmenufont[]       = "PragmataPro Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
